5ea8c27b61aee9c7b91f7e379b3fd165154ab0ce
Francesco <fm2g11@gmail.com>
2022-03-14 22:44:43 +0000

    stuff

--- generate_daily_game.py
+++ generate_daily_game.py
@@ -6,20 +6,25 @@ import time
 import json
 from game import LANGUAGES
 LANGUAGES = sorted(list(LANGUAGES.values()))
-
+import random
+import os
+import string
 
 def job(t):
 	game = {
-		'date': str(date.today()),
+		'id': ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5)),
 		'games': [],
 		'languages': LANGUAGES,
 	}
-
 
 	for i in range(5):
 		game['games'].append(get_word())
 
 	util.write('var GAME = ' + json.dumps(game , indent=2), 'data/game.js')
+	os.system('git add data/game.js')
+	os.system('git commit -m "generating new game"')
+	os.system('git push')
+
 	print('done')
 
 schedule.every().day.at("00:01").do(job,'It is 00:01')
