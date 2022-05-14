14ebac3b1a1e33c5cbbd7fd74121488f6f90ef46
Fran Stap <fran@stap>
2022-05-14 00:01:37 -0400

    generating new game

--- data/game.js
+++ data/game.js
@@ -1,25 +1,25 @@
 var GAME = {
-  "id": "JZB1S",
+  "id": "2JJCN",
   "games": [
     [
-      "kans",
-      "Dutch"
-    ],
-    [
-      "chegadas",
-      "Portuguese"
-    ],
-    [
-      "purified",
-      "English"
-    ],
-    [
-      "pierdan",
+      "blindado",
       "Spanish"
     ],
     [
-      "l\u2019\u00e9ducation",
-      "French"
+      "drenaggio",
+      "Italian"
+    ],
+    [
+      "\u7ba1\u7406\u4f1a\u8ba1\u4e3b\u8981\u4e0e\u7ecf\u7406\u548c\u5916\u90e8\u7528\u6237\u6709\u5173",
+      "Chinese"
+    ],
+    [
+      "begr\u00e6nse",
+      "Danish"
+    ],
+    [
+      "\u1796\u178e\u17cc\u17a2\u17d2\u179c\u17b8\u178a\u17c2\u179b\u1787\u17b6\u1798\u17c9\u17b6\u179f\u17ca\u17b8\u1793\u178f\u17bc\u1785\u178a\u17c2\u179b\u17a2\u17b6\u1785",
+      "Cambodian"
     ]
   ],
   "languages": [
