e6212bd6f7d080e4327c8fa9ae051ade491ba7d3
Fran Stap <fran@stap>
2022-05-26 00:01:06 -0400

    generating new game

--- data/game.js
+++ data/game.js
@@ -1,24 +1,24 @@
 var GAME = {
-  "id": "CXRMY",
+  "id": "Z6EFT",
   "games": [
     [
-      "afasta",
+      "pediu",
       "Portuguese"
     ],
     [
-      "d\u00e9clarer",
-      "French"
+      "fu\u00dft",
+      "German"
     ],
     [
-      "previs\u00e3o",
-      "Portuguese"
+      "\u1790\u17d2\u1784\u17c3\u1780\u17c6\u178e\u17be\u178f\u179a\u1794\u179f\u17cb\u17a2\u17d2\u1793\u1780\u178e\u17b6\u178f\u17d2\u179a\u17bc\u179c\u1794\u17b6\u1793\u1794\u17d2\u179a\u17b6\u179a\u1796\u17d2\u1792\u1787\u17b6\u1790\u17d2\u1784\u17c3\u1782\u17d2\u179a\u17bc",
+      "Cambodian"
     ],
     [
-      "\u05e9\u05de\u05d7\u05d5\u05d1\u05e8",
-      "Hebrew"
+      "sleutelelementen",
+      "Dutch"
     ],
     [
-      "\u0e17\u0e30\u0e40\u0e25\u0e17\u0e23\u0e32\u0e22\u0e2d\u0e32\u0e15\u0e32\u0e01\u0e32\u0e21\u0e32\u0e2d\u0e22\u0e39\u0e48\u0e15\u0e23\u0e07\u0e44\u0e2b\u0e19\u0e43\u0e19\u0e41\u0e1c\u0e19\u0e17\u0e35\u0e48",
+      "\u0e40\u0e21\u0e40\u0e1b\u0e34\u0e25",
       "Thai"
     ]
   ],
