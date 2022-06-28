e5a64068a58efd426913ff8de25ff1afb945db78
Fran Stap <fran@stap>
2022-06-28 00:01:43 -0400

    generating new game

--- data/game.js
+++ data/game.js
@@ -1,25 +1,25 @@
 var GAME = {
-  "id": "KKSUW",
+  "id": "L5CLK",
   "games": [
     [
-      "macam",
+      "bu\u1ed5i",
+      "Vietnamese"
+    ],
+    [
+      "transporteer",
+      "Dutch"
+    ],
+    [
+      "prenumeranter",
+      "Swedish"
+    ],
+    [
+      "koptak",
+      "Hungarian"
+    ],
+    [
+      "menyorok",
       "Malay"
-    ],
-    [
-      "plasmodios",
-      "Spanish"
-    ],
-    [
-      "l\u00e4hiv\u00e4t",
-      "Finnish"
-    ],
-    [
-      "\u0e27\u0e35\u0e2d\u0e32\u0e23\u0e4c\u0e40\u0e14\u0e2d\u0e30\u0e41\u0e0a\u0e21\u0e40\u0e1b\u0e35\u0e22\u0e19\u0e2a\u0e4c",
-      "Thai"
-    ],
-    [
-      "\u0e14\u0e34\u0e2a\u0e04\u0e31\u0e1f\u0e40\u0e27\u0e2d\u0e23\u0e35\u0e48",
-      "Thai"
     ]
   ],
   "languages": [
