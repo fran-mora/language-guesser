e737b817dce91d7043923f250c81b26e8adaa46e
Fran Stap <fran@stap>
2022-07-11 00:01:47 -0400

    generating new game

--- data/game.js
+++ data/game.js
@@ -1,25 +1,25 @@
 var GAME = {
-  "id": "EQOEG",
+  "id": "AT1TN",
   "games": [
     [
-      "lauloin",
-      "Finnish"
+      "abschie\u00dfen",
+      "German"
     ],
     [
-      "elekcji",
-      "Polish"
+      "\u1794\u17d2\u179a\u179f\u17b7\u1791\u17d2\u1792\u17b7\u1797\u17b6\u1796\u1793\u17c3\u1782\u17c4\u179b\u179b\u1791\u17d2\u1792\u17b7\u1793\u17c3\u1780\u17b6\u179a\u1791\u1791\u17bd\u179b\u1781\u17bb\u179f\u178f\u17d2\u179a\u17bc\u179c\u179a\u1794\u179f\u17cb\u179a\u178a\u17d2\u178b\u1798\u1793\u17d2\u179a\u17d2\u178f\u17b8\u1793\u17c5\u1780\u17d2\u1793\u17bb\u1784\u1794\u17d2\u179a\u1791\u17c1\u179f\u1798\u17c9\u17b6\u17a1\u17c1\u179f\u17ca\u17b8",
+      "Cambodian"
     ],
     [
-      "koloni\u00ebn",
-      "Dutch"
+      "kvindens",
+      "Danish"
     ],
     [
-      "\u3068\u306f\u4f55\u3067\u3059\u304b",
-      "Japanese"
+      "\u0645\u064a\u0631\u064a\u062f\u064a\u062b",
+      "Arabic"
     ],
     [
-      "\u015felale",
-      "Turkish"
+      "\u0623\u0628\u064a\u0627\u062a",
+      "Arabic"
     ]
   ],
   "languages": [
