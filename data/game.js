461e948e6fd5cb5ef130dd05afc756f352ce5822
Fran Stap <fran@stap>
2022-07-12 00:01:16 -0400

    generating new game

--- data/game.js
+++ data/game.js
@@ -1,25 +1,25 @@
 var GAME = {
-  "id": "AT1TN",
+  "id": "D7KVV",
   "games": [
     [
-      "abschie\u00dfen",
-      "German"
+      "\u4e3b\u306e\u9855\u6804\u796d\u304c\u884c\u308f\u308c\u308b\u306e\u306f\u4f55\u6708\u3067\u3059\u304b",
+      "Japanese"
     ],
     [
-      "\u1794\u17d2\u179a\u179f\u17b7\u1791\u17d2\u1792\u17b7\u1797\u17b6\u1796\u1793\u17c3\u1782\u17c4\u179b\u179b\u1791\u17d2\u1792\u17b7\u1793\u17c3\u1780\u17b6\u179a\u1791\u1791\u17bd\u179b\u1781\u17bb\u179f\u178f\u17d2\u179a\u17bc\u179c\u179a\u1794\u179f\u17cb\u179a\u178a\u17d2\u178b\u1798\u1793\u17d2\u179a\u17d2\u178f\u17b8\u1793\u17c5\u1780\u17d2\u1793\u17bb\u1784\u1794\u17d2\u179a\u1791\u17c1\u179f\u1798\u17c9\u17b6\u17a1\u17c1\u179f\u17ca\u17b8",
-      "Cambodian"
+      "kauppakomitea",
+      "Finnish"
     ],
     [
-      "kvindens",
-      "Danish"
+      "oppr\u00f8r",
+      "Norwegian"
     ],
     [
-      "\u0645\u064a\u0631\u064a\u062f\u064a\u062b",
-      "Arabic"
+      "prudencea",
+      "Finnish"
     ],
     [
-      "\u0623\u0628\u064a\u0627\u062a",
-      "Arabic"
+      "mencionu",
+      "Portuguese"
     ]
   ],
   "languages": [
