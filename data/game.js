ce4efcbd4998b0747e3cc5323a7c4fb2a2c38dc0
Fran Stap <fran@stap>
2022-07-17 00:01:27 -0400

    generating new game

--- data/game.js
+++ data/game.js
@@ -1,25 +1,25 @@
 var GAME = {
-  "id": "GCA9P",
+  "id": "L8IYX",
   "games": [
     [
-      "captaincy",
-      "English"
+      "st\u00e4llning",
+      "Swedish"
     ],
     [
-      "m\u00e9dicale",
-      "French"
+      "originaram",
+      "Portuguese"
     ],
     [
-      "intersecci\u00f3n",
-      "Spanish"
+      "\u178f\u17be\u1780\u1793\u17d2\u179b\u17c2\u1784\u178e\u17b6",
+      "Cambodian"
     ],
     [
-      "contrairement",
-      "French"
+      "\u0e40\u0e01\u0e21\u0e23\u0e27\u0e21\u0e14\u0e32\u0e23\u0e32\u0e08\u0e30\u0e41\u0e02\u0e48\u0e07\u0e17\u0e35\u0e48\u0e44\u0e2b\u0e19\u0e43\u0e19\u0e1b\u0e35\u0e19\u0e35\u0e49",
+      "Thai"
     ],
     [
-      "escribio",
-      "Spanish"
+      "\u0648\u0634\u0628\u0627\u0643",
+      "Arabic"
     ]
   ],
   "languages": [
