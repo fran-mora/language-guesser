23101b5490d6d05dcdcb9b143624499a4f2e2b96
Fran Stap <fran@stap>
2022-07-19 00:01:19 -0400

    generating new game

--- data/game.js
+++ data/game.js
@@ -1,25 +1,25 @@
 var GAME = {
-  "id": "RMJQ5",
+  "id": "4GOUM",
   "games": [
     [
-      "\u0441\u0442\u0440\u0435\u043b\u043a\u0430",
-      "Russian"
+      "blanca",
+      "Spanish"
     ],
     [
-      "tennismatchen",
+      "immaterialla",
       "Swedish"
     ],
     [
-      "anciente",
-      "Portuguese"
+      "zitten",
+      "Dutch"
     ],
     [
-      "m\u00e9diane",
-      "French"
+      "yatim",
+      "Malay"
     ],
     [
-      "appoint",
-      "English"
+      "\u0e23\u0e27\u0e21\u0e40\u0e1b\u0e47\u0e19\u0e2a\u0e48\u0e27\u0e19\u0e2b\u0e19\u0e36\u0e48\u0e07\u0e02\u0e2d\u0e07\u0e2a\u0e2b\u0e23\u0e31\u0e10\u0e2d\u0e40\u0e21\u0e23\u0e34\u0e01\u0e32\u0e40\u0e21\u0e37\u0e48\u0e2d\u0e44\u0e2b\u0e23\u0e48",
+      "Thai"
     ]
   ],
   "languages": [
