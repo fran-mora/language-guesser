560462d5e86149e29516ed1a04cc1a51997c7ee0
Fran Stap <fran@stap>
2022-07-20 00:01:45 -0400

    generating new game

--- data/game.js
+++ data/game.js
@@ -1,25 +1,25 @@
 var GAME = {
-  "id": "4GOUM",
+  "id": "H4LY8",
   "games": [
     [
-      "blanca",
-      "Spanish"
+      "punkterekord",
+      "German"
     ],
     [
-      "immaterialla",
+      "\u0627\u0644\u062d\u0631\u0643\u0629",
+      "Arabic"
+    ],
+    [
+      "\u0644\u0622\u0644\u0629",
+      "Arabic"
+    ],
+    [
+      "allokationen",
       "Swedish"
     ],
     [
-      "zitten",
-      "Dutch"
-    ],
-    [
-      "yatim",
-      "Malay"
-    ],
-    [
-      "\u0e23\u0e27\u0e21\u0e40\u0e1b\u0e47\u0e19\u0e2a\u0e48\u0e27\u0e19\u0e2b\u0e19\u0e36\u0e48\u0e07\u0e02\u0e2d\u0e07\u0e2a\u0e2b\u0e23\u0e31\u0e10\u0e2d\u0e40\u0e21\u0e23\u0e34\u0e01\u0e32\u0e40\u0e21\u0e37\u0e48\u0e2d\u0e44\u0e2b\u0e23\u0e48",
-      "Thai"
+      "stanem",
+      "Polish"
     ]
   ],
   "languages": [
