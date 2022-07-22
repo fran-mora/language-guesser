c1a1e2484a16dd8c56f4c6ba098ae20f171c9bd5
Fran Stap <fran@stap>
2022-07-22 00:01:36 -0400

    generating new game

--- data/game.js
+++ data/game.js
@@ -1,25 +1,25 @@
 var GAME = {
-  "id": "0FPUR",
+  "id": "XSCH9",
   "games": [
     [
-      "int\u00e4kten",
+      "angelia",
+      "Finnish"
+    ],
+    [
+      "javasolt",
+      "Hungarian"
+    ],
+    [
+      "encik",
+      "Malay"
+    ],
+    [
+      "kuninkaallisen",
+      "Finnish"
+    ],
+    [
+      "snabbast",
       "Swedish"
-    ],
-    [
-      "\u178a\u17c2\u179b\u1794\u17b6\u1793\u179f\u179a\u179f\u17c1\u179a\u1791\u17d2\u179a\u17b9\u179f\u17d2\u178f\u17b8\u17a2\u1794\u17cb\u179a\u17c6\u17a2\u17c1\u1798\u17b8\u179b\u17a2\u17b7\u1798",
-      "Cambodian"
-    ],
-    [
-      "appartiene",
-      "Italian"
-    ],
-    [
-      "fonksiyonlar\u0131",
-      "Turkish"
-    ],
-    [
-      "noches",
-      "Spanish"
     ]
   ],
   "languages": [
