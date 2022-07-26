c252c06078a68b0d14e3bd4921c68ab26a83c7a3
Fran Stap <fran@stap>
2022-07-26 00:01:16 -0400

    generating new game

--- data/game.js
+++ data/game.js
@@ -1,25 +1,25 @@
 var GAME = {
-  "id": "Y9V8E",
+  "id": "Q6NOS",
   "games": [
     [
-      "d\u00e9coupe",
-      "French"
+      "\u0e01\u0e32\u0e23\u0e22\u0e01\u0e1e\u0e25\u0e02\u0e36\u0e49\u0e19\u0e1a\u0e01\u0e17\u0e35\u0e48\u0e19\u0e2d\u0e23\u0e4c\u0e21\u0e47\u0e2d\u0e07\u0e14\u0e35\u0e40\u0e23\u0e34\u0e48\u0e21\u0e02\u0e36\u0e49\u0e19\u0e40\u0e27\u0e25\u0e32\u0e44\u0e2b\u0e19",
+      "Thai"
     ],
     [
-      "polg\u00e1rh\u00e1bor\u00fat",
-      "Hungarian"
+      "korthuset",
+      "Danish"
     ],
     [
-      "\u05d2\u05e8\u05e2\u05d9\u05e0\u05d9\u05d5\u05ea",
-      "Hebrew"
+      "berechtigten",
+      "German"
     ],
     [
-      "\ud3ec\uc2a4\ud2b8\uc758",
-      "Korean"
+      "kejohananan",
+      "Malay"
     ],
     [
-      "\u0642\u0630\u0631\u061f",
-      "Arabic"
+      "\u178f\u17be\u17a2\u17d2\u179c\u17b8\u1791\u17c5\u1787\u17b6\u1780\u17b6\u179a\u179c\u17b7\u179b\u178f\u17d2\u179a\u17a1\u1794\u17cb\u179a\u1794\u179f\u17cb",
+      "Cambodian"
     ]
   ],
   "languages": [
