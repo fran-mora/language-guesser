7275d377df6023e5e75411daebb78eb4170b205d
Fran Stap <fran@stap>
2022-10-02 00:01:52 -0400

    generating new game

--- data/game.js
+++ data/game.js
@@ -1,25 +1,25 @@
 var GAME = {
-  "id": "XWPPL",
+  "id": "C8Q2V",
   "games": [
     [
-      "planets",
-      "English"
+      "\u0e01\u0e32\u0e23\u0e4c\u0e15\u0e39\u0e19\u0e17\u0e35\u0e48\u0e2a\u0e23\u0e49\u0e32\u0e07\u0e42\u0e14\u0e22",
+      "Thai"
     ],
     [
-      "coeloom",
-      "Dutch"
+      "\u4e00\u7f50",
+      "Chinese"
     ],
     [
-      "\u178f\u17be\u17a2\u17d2\u179c\u17b8\u1787\u17b6\u1797\u17b6\u1796\u1781\u17bb\u179f\u1782\u17d2\u1793\u17b6\u179a\u179c\u17b6\u1784",
-      "Cambodian"
+      "\u0447\u0435\u043b\u043e\u0432\u0435\u043a\u0443",
+      "Russian"
     ],
     [
-      "\u30b8\u30e3\u30f3\u30b0\u30eb\u5927\u5e1d",
-      "Japanese"
-    ],
-    [
-      "v\u00e4derkanalen",
+      "inflytelserik",
       "Swedish"
+    ],
+    [
+      "publiques",
+      "French"
     ]
   ],
   "languages": [
