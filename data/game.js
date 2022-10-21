0678db3864f7d2587edcc507f6643b59c503074c
Fran Stap <fran@stap>
2022-10-21 00:01:09 -0400

    generating new game

--- data/game.js
+++ data/game.js
@@ -1,25 +1,25 @@
 var GAME = {
-  "id": "NJLBD",
+  "id": "G99JF",
   "games": [
     [
-      "syvin",
-      "Finnish"
-    ],
-    [
-      "comprennent",
-      "French"
-    ],
-    [
-      "\ud1a0\ub9c8\uc2a4\uac00",
+      "\uc138\uae08\uc785\ub2c8\ub2e4\ub97c",
       "Korean"
     ],
     [
-      "\uad6c\uc131\uc740",
-      "Korean"
+      "\u1794\u17d2\u179a\u17b6\u1794\u17cb",
+      "Cambodian"
     ],
     [
-      "valts",
-      "Swedish"
+      "munique",
+      "Portuguese"
+    ],
+    [
+      "\u30ef\u30f3\u30b9\u30fb\u30a2\u30dd\u30f3\u30fb\u30a2\u30fb\u30bf\u30a4\u30e0\u30fb\u30a4\u30f3\u30fb\u30ef\u30f3\u30c0\u30fc\u30e9\u30f3\u30c9\u306f\u4f55\u3067\u3059\u304b",
+      "Japanese"
+    ],
+    [
+      "\u306f\u3044\u3064\u30ef\u30fc\u30eb\u30c9\u30b7\u30ea\u30fc\u30ba\u3067\u512a\u52dd\u3057\u307e\u3057\u305f\u304b",
+      "Japanese"
     ]
   ],
   "languages": [
