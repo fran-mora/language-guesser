b99b05520b80e8bc45fab9a0ddc79b55bb766064
Fran Stap <fran@stap>
2022-10-22 00:01:23 -0400

    generating new game

--- data/game.js
+++ data/game.js
@@ -1,25 +1,25 @@
 var GAME = {
-  "id": "G99JF",
+  "id": "RCKH2",
   "games": [
     [
-      "\uc138\uae08\uc785\ub2c8\ub2e4\ub97c",
-      "Korean"
+      "zaleg\u0142o\u015b\u0107",
+      "Polish"
     ],
     [
-      "\u1794\u17d2\u179a\u17b6\u1794\u17cb",
-      "Cambodian"
+      "brit\u00e1nica",
+      "Spanish"
     ],
     [
-      "munique",
+      "baas",
+      "Dutch"
+    ],
+    [
+      "localiza\u00e7\u00f5es",
       "Portuguese"
     ],
     [
-      "\u30ef\u30f3\u30b9\u30fb\u30a2\u30dd\u30f3\u30fb\u30a2\u30fb\u30bf\u30a4\u30e0\u30fb\u30a4\u30f3\u30fb\u30ef\u30f3\u30c0\u30fc\u30e9\u30f3\u30c9\u306f\u4f55\u3067\u3059\u304b",
-      "Japanese"
-    ],
-    [
-      "\u306f\u3044\u3064\u30ef\u30fc\u30eb\u30c9\u30b7\u30ea\u30fc\u30ba\u3067\u512a\u52dd\u3057\u307e\u3057\u305f\u304b",
-      "Japanese"
+      "armist\u00edcio",
+      "Portuguese"
     ]
   ],
   "languages": [
