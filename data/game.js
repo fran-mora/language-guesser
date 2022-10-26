5c395deb51d285c9986ee0a6d310096b98afd504
Fran Stap <fran@stap>
2022-10-26 00:01:25 -0400

    generating new game

--- data/game.js
+++ data/game.js
@@ -1,25 +1,25 @@
 var GAME = {
-  "id": "OUAPN",
+  "id": "VZ69J",
   "games": [
     [
-      "kar\u015f\u0131yd\u0131",
-      "Turkish"
+      "philosophe",
+      "French"
     ],
     [
-      "\ub450\ub824\uc6b4",
-      "Korean"
-    ],
-    [
-      "\ud3f4\ub780\ub4dc\uc5d0",
-      "Korean"
-    ],
-    [
-      "\u05ea\u05e7\u05e6\u05d9\u05e8",
+      "\u05d4\u05e4\u05d9\u05e0\u05dc\u05d4",
       "Hebrew"
     ],
     [
-      "ma\u00f1ana",
-      "Spanish"
+      "grootste",
+      "Dutch"
+    ],
+    [
+      "tsjechi\u00eb",
+      "Dutch"
+    ],
+    [
+      "\u0e41\u0e1e\u0e19\u0e40\u0e18\u0e2d\u0e23\u0e4c\u0e21\u0e32\u0e08\u0e32\u0e01\u0e44\u0e2b\u0e19",
+      "Thai"
     ]
   ],
   "languages": [
