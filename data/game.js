37d5fc2da9f95b4ed0a6a2259d09d97914679c3f
Fran Stap <fran@stap>
2022-11-02 00:01:12 -0400

    generating new game

--- data/game.js
+++ data/game.js
@@ -1,25 +1,25 @@
 var GAME = {
-  "id": "VNMIM",
+  "id": "SMG98",
   "games": [
     [
-      "\u0e40\u0e23\u0e37\u0e48\u0e2d\u0e07\u0e22\u0e48\u0e2d\u0e02\u0e2d\u0e07\u0e2b\u0e19\u0e31\u0e07\u0e2a\u0e37\u0e2d\u0e40\u0e23\u0e37\u0e48\u0e2d\u0e07",
+      "\u062a\u064a\u062a\u064a\u0643\u0627\u0643\u0627",
+      "Arabic"
+    ],
+    [
+      "\u5230\u5357\u5361\u7f57\u6765\u7eb3\u5dde\u7684",
+      "Chinese"
+    ],
+    [
+      "krall\u0131\u011f\u0131n\u0131",
+      "Turkish"
+    ],
+    [
+      "\u0e0a\u0e19\u0e30\u0e2a\u0e41\u0e15\u0e19\u0e25\u0e35\u0e22\u0e4c\u0e04\u0e31\u0e1e\u0e40\u0e21\u0e37\u0e48\u0e2d\u0e44\u0e2b\u0e23\u0e48",
       "Thai"
     ],
     [
-      "placeringer",
-      "Danish"
-    ],
-    [
-      "merger",
-      "English"
-    ],
-    [
-      "talsmann",
-      "Norwegian"
-    ],
-    [
-      "modstandsbev\u00e6gelse",
-      "Danish"
+      "\u05d7\u05de\u05d9\u05e9\u05d9\u05dd",
+      "Hebrew"
     ]
   ],
   "languages": [
