8a088cb06642a0811e8155b2779dcf863521f69f
Fran Stap <fran@stap>
2022-11-10 00:01:14 -0500

    generating new game

--- data/game.js
+++ data/game.js
@@ -1,25 +1,25 @@
 var GAME = {
-  "id": "39ZK9",
+  "id": "JPG58",
   "games": [
     [
-      "indsendelse",
-      "Danish"
-    ],
-    [
-      "surgeries",
+      "colonists",
       "English"
     ],
     [
-      "kedaulatan",
-      "Malay"
+      "\u17a2\u17d2\u179c\u17b8\u178a\u17c2\u179b\u1787\u17b6\u1785\u17c6\u1793\u17bd\u1793\u1794\u17d2\u179a\u1787\u17b6\u1787\u1793\u1793\u17c3\u1794\u17c9\u17bc\u179b\u1781\u17b6\u1784\u1787\u17be\u1784",
+      "Cambodian"
     ],
     [
-      "viktigheten",
-      "Norwegian"
+      "coniglio",
+      "Italian"
     ],
     [
-      "ljusstr\u00e5larna",
-      "Swedish"
+      "vezettet",
+      "Hungarian"
+    ],
+    [
+      "\u6930\u5b50\u957f\u5728\u4ec0\u4e48\u6811\u4e0a",
+      "Chinese"
     ]
   ],
   "languages": [
