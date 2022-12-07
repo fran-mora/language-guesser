da682677bba8342a19270b954afc9311a59ef977
Fran Stap <fran@stap>
2022-12-07 00:01:06 -0500

    generating new game

--- data/game.js
+++ data/game.js
@@ -1,24 +1,24 @@
 var GAME = {
-  "id": "EE4IM",
+  "id": "X32LM",
   "games": [
     [
-      "\u0432\u044b\u0441\u0442\u0440\u0435\u043b",
-      "Russian"
+      "\uc81c\uc784\uc2a4\uac00",
+      "Korean"
     ],
     [
-      "\u7b2c\u4e00\u90e8\u94f6\u6cb3\u62a4\u536b\u961f\u7535\u5f71\u662f\u4f55\u65f6\u51fa\u54c1\u7684",
+      "\u0e40\u0e1b\u0e47\u0e19\u0e01\u0e32\u0e23\u0e15\u0e48\u0e2d\u0e2a\u0e39\u0e49\u0e23\u0e30\u0e2b\u0e27\u0e48\u0e32\u0e07\u0e43\u0e04\u0e23",
+      "Thai"
+    ],
+    [
+      "\u4e0a\u4e00\u90e8\u661f\u9645\u8ff7\u822a\u662f\u4f55\u65f6\u95ee\u4e16\u7684",
       "Chinese"
     ],
     [
-      "ocurrio",
-      "Spanish"
+      "ledelsesregnskab",
+      "Danish"
     ],
     [
-      "liikaa",
-      "Finnish"
-    ],
-    [
-      "aleluia",
+      "florda",
       "Portuguese"
     ]
   ],
