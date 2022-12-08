61c1b2d0819cd3d477071f1ee037242c442dbcbf
Fran Stap <fran@stap>
2022-12-08 00:01:21 -0500

    generating new game

--- data/game.js
+++ data/game.js
@@ -1,25 +1,25 @@
 var GAME = {
-  "id": "X32LM",
+  "id": "98ALO",
   "games": [
     [
-      "\uc81c\uc784\uc2a4\uac00",
-      "Korean"
-    ],
-    [
-      "\u0e40\u0e1b\u0e47\u0e19\u0e01\u0e32\u0e23\u0e15\u0e48\u0e2d\u0e2a\u0e39\u0e49\u0e23\u0e30\u0e2b\u0e27\u0e48\u0e32\u0e07\u0e43\u0e04\u0e23",
-      "Thai"
-    ],
-    [
-      "\u4e0a\u4e00\u90e8\u661f\u9645\u8ff7\u822a\u662f\u4f55\u65f6\u95ee\u4e16\u7684",
-      "Chinese"
-    ],
-    [
-      "ledelsesregnskab",
-      "Danish"
-    ],
-    [
-      "florda",
+      "ilhas",
       "Portuguese"
+    ],
+    [
+      "yadigar",
+      "Turkish"
+    ],
+    [
+      "b\u00eates",
+      "French"
+    ],
+    [
+      "krigsfangene",
+      "Norwegian"
+    ],
+    [
+      "\uc131\ubd84\uc774\ub2e4",
+      "Korean"
     ]
   ],
   "languages": [
